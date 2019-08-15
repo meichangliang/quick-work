d3db23ea0cbcbe1c1ceae32dbb61b2ad1982d9af
Mark <meichangliang@outlook.com>
2019-08-15 16:54:49 +0800

    hen nice

--- src/cmder_demo.ts
+++ src/cmder_demo.ts
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 const program = require('commander');
 program
-    .option('-d, --debug', 'output extra debugging')
-    .option('-s, --small', 'small pizza size')
-    .option('-p, --pizza-type <type>', 'flavour of pizza');
+  .option('-d, --debug', 'output extra debugging')
+  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza');
 program.parse(process.argv);
-if (program.debug) console.log(program.opts());
-console.log('pizza details:');
-if (program.small) console.log('- small pizza size');
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
+if (program.debug) {
+  console.info(program.opts());
+}
+console.info('pizza details:');
+if (program.small) {
+  console.info('- small pizza size');
+}
+if (program.pizzaType) {
+  console.info(`- ${program.pizzaType}`);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
-const program = require('commander');
+const commander = require('commander');
 const inquirer = require('inquirer');
 
 const initAction = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: '请输入项目名称:',
-                name: 'name'
-            }
-        ])
-        .then(answers => {
-            console.log('项目名为：', answers.name);
-            console.log('正在拷贝项目，请稍等');
-        });
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: '请输入项目名称:',
+        name: 'name',
+      },
+    ])
+    .then((answers) => {
+      console.info('项目名为：', answers.name);
+      console.info('正在拷贝项目，请稍等');
+    });
 };
-program.version(require('../package.json').version);
-program
-    .command('init')
-    .description('创建项目')
-    .action(initAction);
-program.parse(process.argv);
+commander.version(require('../package.json').version);
+commander
+  .command('init')
+  .description('创建项目')
+  .action(initAction);
+commander.parse(process.argv);
+
