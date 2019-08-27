48616c5ce316e006da80ae73efadd6015d5b2c44
Mark <meichangliang@outlook.com>
2019-08-27 11:44:41 +0800

    update~

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const commander: any;
-declare const inquirer: any;
-declare const initAction: () => void;
-declare const object: {
-    "foo": string;
-    "baz": number;
-    "qux-lorem": boolean;
-};
