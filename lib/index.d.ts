25a8f4ca8354c8099e3f7f15d2f5a5f1cfd738a8
Mark <meichangliang@outlook.com>
2019-08-27 11:41:54 +0800

    追求完美的最佳实践

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+declare const commander: any;
+declare const inquirer: any;
+declare const initAction: () => void;
+declare const object: {
+    "foo": string;
+    "baz": number;
+    "qux-lorem": boolean;
+};
