8a3a26a6ec472d0dd0bf78d28b13248346cabc5c
Mark <meichangliang@outlook.com>
2019-08-27 16:26:45 +0800

    交互一下 `

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,5 @@
-const program = require("commander");
-
-program
-  .version("0.1.0")
-  .option("-p, --peppers", "Add peppers")
-  .option("-P, --pineapple", "Add pineapple")
-  .option("-b, --bbq-sauce", "Add bbq sauce")
-  .option(
-    "-c, --cheese [type]",
-    "Add the specified type of cheese [marble]",
-    "marble"
-  )
-  .parse(process.argv);
-
-console.info("you ordered a pizza with:");
-if(program.peppers){
-
-  console.log("  - peppers");
-
-}
-if(program.pineapple){
-
-  console.log("  - pineapple");
-
-}
-if(program.bbqSauce){
-
-  console.log("  - bbq");
-
-}
-console.log("  - %s cheese", program.cheese);
+const program  = require("commander");
+const npmInfo = require("../package");
+// program.version(npmInfo.version, "-v", "--version").parse(process.argv);
+program.version(npmInfo.version).parse(process.argv)
+;
