101fd46c20e580e1c2f84af4043518420a1a41fe
Mark <meichangliang@outlook.com>
2019-09-18 13:47:36 +0800

    让代码看起来更舒服系列 ~

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 const program = require("commander");
 const npmInfo = require("../package");
-import {mkdir} from "./utils/mkdir";
+import { mkdir } from "./utils/mkdir";
 const inquirer = require("inquirer");
-import {buildProject} from "./step/build";
+import { buildProject } from "./step/build";
 
 //不管三七二十一.先创建一个缓存目录
 const os = require("os");
@@ -10,12 +10,11 @@ const homedir = os.homedir();
 const localPath = `${homedir}/quick-work`;
 mkdir(localPath);
 
-
 //基础部分
 program.version(npmInfo.version).parse(process.argv);
 
 //问答部分
-import {depotLoad} from "./step/depotLoad";
+import { depotLoad } from "./step/depotLoad";
 const projectQA: Array<object> = [
   {
     type: "list",
@@ -49,7 +48,6 @@ const projectQA: Array<object> = [
   },
 ];
 
-
 //开始下一步骤的提问: 编辑器\是否以当前目录为根目录
 const developQA: Array<object> = [
   {
@@ -71,7 +69,6 @@ const developQA: Array<object> = [
   },
 ];
 
-
 //项目的名字
 
 const projectNameQA: Array<object> = [
@@ -83,81 +80,51 @@ const projectNameQA: Array<object> = [
   },
 ];
 
-
 //构建交互界面
 const _start = (): Promise<any> => {
-
   return new Promise((resolve): any => {
-
     inquirer.prompt(projectQA).then((answers: any) => {
-
       depotLoad(answers, (param: string) => {
-
         resolve(param);
-
       });
-
     });
-
   });
-
-
 };
 
 const _develop = (): Promise<any> => {
-
   return new Promise((resolve): any => {
-
     inquirer.prompt(developQA).then((pram: any) => {
-
       resolve(pram);
-
     });
-
   });
-
 };
-
 
 const _projectName = (): Promise<any> => {
-
   return new Promise((resolve): any => {
-
     inquirer.prompt(projectNameQA).then((pram: any) => {
-
       resolve(pram);
-
     });
-
   });
-
 };
-
 
 //run
 const main = async (): Promise<any> => {
-
   const projectPath = await _start();
   const developResult = await _develop();
   let QAresult = {
     ...developResult,
   };
-  if(!developResult.rootDir){
-
+  if (!developResult.rootDir) {
     const projectName = await _projectName();
     QAresult = {
       ...QAresult,
       ...projectName,
     };
-
   }
 
   QAresult.projectPath = projectPath;
   QAresult.buildPath = process.cwd();
   buildProject(QAresult);
-
 };
 
 main();
-
-
